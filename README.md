b46832527fa87b61da45b7c2c328c203973cddaf
gabriels997 <gabriel.s997@outlook.com>
2022-11-08 09:19:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio-em-progresso
+ Portfolio esboço
---
+++ script.js
@@ -0,0 +1,49 @@
+function showConhec() {
+    const conhecimentos = document.querySelectorAll('.js-conhecimentos dd');
+    conhecimentos[0].nextElementSibling.classList.add('ativo');
+
+    function showContent() {
+        this.nextElementSibling.classList.toggle('ativo')
+    }
+
+    conhecimentos.forEach((item) => {
+        item.addEventListener('click', showContent)
+    });
+}
+showConhec();
+
+function scrollToSec() {
+    const linkInterno = document.querySelectorAll('.js-nav a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    }
+
+    linkInterno.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+};
+scrollToSec();
+
+const section = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight * 0.8;
+section[0].classList.add('animar');
+
+ 
+function animaScroll() {
+    section.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const sectionVisible = (sectionTop - windowMetade) < 0;
+        if(sectionVisible) {
+            section.classList.add('animar')
+        }
+    });
+}
+
+window.addEventListener('scroll', animaScroll)
